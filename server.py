734d3bb12b0828519b66148ca78ff8b2c6b2e8e8
hisham waleed karam <hisham.karam@cartologic.com>
2017-05-14 09:55:29 +0200

    change uwsgi to cherrrypy web server

---
+++ server.py
@@ -0,0 +1,19 @@
+# Import CherryPy
+import cherrypy
+if __name__ == '__main__':
+    # Mount the application
+    cherrypy.tree.graft(application, "/")
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+    # Configure the server object
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 8000
+    server.thread_pool = 30
+    # For SSL Support server.ssl_module = 'pyopenssl' server.ssl_certificate =
+    # 'ssl/certificate.crt' server.ssl_private_key = 'ssl/private.key'
+    # server.ssl_certificate_chain = 'ssl/bundle.crt' Subscribe this server
+    server.subscribe()
+    cherrypy.engine.start()
+    cherrypy.engine.block()
