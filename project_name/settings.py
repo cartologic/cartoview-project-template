da7299b0ab00f1d8b8e25ec999cfc64b759766fa
hisham waleed karam <hisham.karam@cartologic.com>
2018-07-30 20:50:17 +0200

    update settings.py

--- project_name/settings.py
+++ project_name/settings.py
@@ -4,15 +4,15 @@ try:
 except:
     pass
 import os
-import cartoview
 
+from cartoview.app_manager.settings import load_apps
+from cartoview.settings import *
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-cartoview_settings_path = os.path.join(
-    os.path.dirname(cartoview.__file__), 'settings.py')
-execfile(cartoview_settings_path)
-# bower static files
+APPS_DIR = os.path.abspath(os.path.join(BASE_DIR, "apps"))
+PENDING_APPS = os.path.join(APPS_DIR, "pendingOperation.yml")
+INSTALLED_APPS += load_apps()
 STATICFILES_DIRS += [os.path.join(PROJECT_DIR, "static"), ]
 # django Media Section
 # uncomment the following if you want your files out of geonode folder
@@ -25,11 +25,3 @@ try:
     from .local_settings import *
 except:
     pass
-
-if 'datastore' in DATABASES:
-    OGC_SERVER['default']['DATASTORE'] = 'datastore'
-
-MIDDLEWARE_CLASSES += ("django.middleware.gzip.GZipMiddleware",)
-
-if 'geonode.geoserver' in INSTALLED_APPS and "LOCAL_GEOSERVER" in locals() and LOCAL_GEOSERVER in MAP_BASELAYERS:
-    LOCAL_GEOSERVER["source"]["url"] = OGC_SERVER['default']['PUBLIC_LOCATION'] + "wms"
