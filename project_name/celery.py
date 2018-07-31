5556eb91a6fe14f5e476b2d70ad6aa9aef4064b3
hisham waleed karam <hisham.karam@cartologic.com>
2018-07-31 15:41:36 +0200

    improve docker-compose file
    add missing services

---
+++ project_name/celery.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{project_name}}.settings')
+
+app = Celery('cartoview')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings', namespace="CELERY")
+app.autodiscover_tasks()
