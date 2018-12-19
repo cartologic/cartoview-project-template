84a468cef8525102aab96a65e89740397010b34e
hisham waleed karam <hisham.karam@cartologic.com>
2018-12-19 11:02:35 +0200

    fix celery app

--- project_name/celery.py
+++ project_name/celery.py
@@ -5,9 +5,9 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cartoview.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{project_name}}.settings')
 
-app = Celery('cartoview')
+app = Celery('{{project_name}}')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
