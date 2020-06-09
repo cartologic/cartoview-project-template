782c9f52429e6535040ed1d2da5bddbf84170cc3
khaled <bokae97@gmail.com>
2020-06-10 00:00:36 +0200

    installation requirements setup shell script

--- docker/cartoview/scripts/setup.sh
+++ docker/cartoview/scripts/setup.sh
@@ -1,34 +1,60 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-apt-get update -y && apt-get install wget gnupg -y
+# Bash "strict mode", to help catch problems and bugs in the shell
+# script. Every bash script you write should include this. See
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/ for
+# details.
+set -euo pipefail
 
-# add postgres client latest
-touch /etc/apt/sources.list.d/pgdg.list &&
-	echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" >>/etc/apt/sources.list.d/pgdg.list &&
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+# Tell apt-get we're never going to be able to give manual
+# feedback:
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package listing, so we know what package exist:
+apt-get update
+
+# Install security updates:
+apt-get -y upgrade
+
+# Install wget and gnupg
+apt-get install wget gnupg -y
+
+# Enable postgresql-client-11.x
+echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
 # add gdal repo
-echo "deb http://http.us.debian.org/debian buster main non-free contrib" >>/etc/apt/sources.list
+#echo "deb http://http.us.debian.org/debian buster main non-free contrib" >>/etc/apt/sources.list
+#add-apt-repository ppa:ubuntugis/ppa && apt-get update
 
-# geonode required libraries
-apt-get update -y && apt-get install -y \
-	build-essential gcc git \
-	libxml2-dev libxslt1-dev libxslt-dev python-dev \
-	gettext sqlite3 \
-	python-lxml \
-	postgresql-client libpq-dev python-psycopg2 \
-	python-pil \
-	python-ldap \
-	libmemcached-dev libsasl2-dev \
-	python-pylibmc \
-	gdal-bin libgdal-dev libgeos-dev \
-	--no-install-recommends
+# This section is borrowed from the official Django image but adds GDAL and others
+apt-get install -y \
+                gcc \
+                gettext \
+                postgresql-client libpq-dev \
+                sqlite3 \
+                python3-psycopg2 \
+                python3-lxml \
+                python3-dev libgdal-dev \
+                python3-ldap \
+                libmemcached-dev libsasl2-dev zlib1g-dev \
+                python3-pylibmc \
+                libgeos-dev gdal-bin \
+        --no-install-recommends && rm -rf /var/lib/apt/lists/*
 
-# update python pip version
-pip install --upgrade pip
+# Upgrade pip
+pip install pip==20.1
 
-# install python gdal stable
-pip install GDAL==2.3.2
+# Update C env vars so compiler can find gdal
+CPLUS_INCLUDE_PATH=/usr/include/gdal
+C_INCLUDE_PATH=/usr/include/gdal
+
+#let's install pygdal wheels compatible with the provided libgdal-dev
+gdal-config --version | cut -c 1-5 | xargs -I % pip install 'pygdal>=%.0,<=%.999'
+
+# install geoip-bin
+printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
+apt-get update && apt-get install -y geoip-bin
 
 # install cartoview
 cd /tmp
@@ -45,10 +71,8 @@ pip install -r extra_req.txt
 # cleanup image
 rm -rf ~/.cache/pip
 rm -rf /root/.cache
-apt-get purge --auto-remove -y gcc libgdal-dev libsasl2-dev \
-	zlib1g-dev python-dev build-essential
 apt autoremove --purge -y && apt autoclean -y && apt-get clean -y
 rm -rf /var/lib/apt/lists/* && apt-get clean -y &&
-	rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+        rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 echo "Yes, do as I say!" | apt-get remove --force-yes login &&
-	dpkg --remove --force-depends wget
+        dpkg --remove --force-depends wget
