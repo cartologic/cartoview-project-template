202f0e88c9b4d84eed5c4d4cb5022bd2de41e290
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2020-08-23 23:39:37 +0200

    upgrad to cartoview 1.30 & geonode 3

--- docker/cartoview/scripts/setup.sh
+++ docker/cartoview/scripts/setup.sh
@@ -1,38 +1,54 @@
 #!/usr/bin/env bash
 
-apt-get update -y && apt-get install wget gnupg -y
 
-# add postgres client latest
-touch /etc/apt/sources.list.d/pgdg.list &&
-	echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" >>/etc/apt/sources.list.d/pgdg.list &&
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+# Update the package listing, so we know what package exist:
+apt-get update -y
+
+# Install security updates:
+apt-get -y upgrade
+
+apt-get install wget gnupg -y
+
+# add postgres client 11.2
+echo "deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
 # add gdal repo
 echo "deb http://http.us.debian.org/debian buster main non-free contrib" >>/etc/apt/sources.list
 
 # geonode required libraries
-apt-get update -y && apt-get install -y \
-	build-essential gcc git \
-	libxml2-dev libxslt1-dev libxslt-dev python-dev \
-	gettext sqlite3 \
-	python-lxml \
-	postgresql-client libpq-dev python-psycopg2 \
-	python-pil \
-	python-ldap \
-	libmemcached-dev libsasl2-dev \
-	python-pylibmc \
-	gdal-bin libgdal-dev libgeos-dev \
-	--no-install-recommends
+apt-get update && apt-get install -y \
+		gcc zip \
+		gettext \
+		postgresql-client-11 libpq-dev \
+		sqlite3 spatialite-bin libsqlite3-mod-spatialite \
+                python3-gdal python3-psycopg2 python3-ldap \
+                python3-pil python3-lxml python3-pylibmc \
+                python3-dev libgdal-dev \
+                libxml2 libxml2-dev libxslt1-dev zlib1g-dev libjpeg-dev \
+                libmemcached-dev libsasl2-dev \
+                libldap2-dev libsasl2-dev \
+                uwsgi uwsgi-plugin-python3 \
+	--no-install-recommends && rm -rf /var/lib/apt/lists/*
 
 # update python pip version
 pip install --upgrade pip
 
-# install python gdal stable
-pip install GDAL==2.3.2
+# Update C env vars so compiler can find gdal
+CPLUS_INCLUDE_PATH=/usr/include/gdal
+C_INCLUDE_PATH=/usr/include/gdal
+
+#let's install pygdal wheels compatible with the provided libgdal-dev
+gdal-config --version | cut -c 1-5 | xargs -I % pip install 'pygdal>=%.0,<=%.999'
+
+# install geoip-bin
+printf "deb http://archive.debian.org/debian/ jessie main\ndeb-src http://archive.debian.org/debian/ jessie main\ndeb http://security.debian.org jessie/updates main\ndeb-src http://security.debian.org jessie/updates main" > /etc/apt/sources.list
+apt-get update && apt-get install -y geoip-bin
+
 
 # install cartoview
 cd /tmp
-git clone https://github.com/cartologic/cartoview.git -b 1.10.x
+git clone https://github.com/cartologic/cartoview.git
 cd cartoview
 pip install .
 cd /tmp
--- server.py
+++ server.py
@@ -1,22 +1,32 @@
-# Import CherryPy
-from {{project_name}}.wsgi import application
+from django.conf import settings
 import cherrypy
+from {{project_name}}.wsgi import application
+
+from cherrypy import tools
 if __name__ == '__main__':
-    # Mount the application
-    cherrypy.tree.graft(application, "/")
-    # Unsubscribe the default server
-    cherrypy.server.unsubscribe()
-    # Instantiate a new server object
-    server = cherrypy._cpserver.Server()
-    # Configure the server object
-    server.socket_host = "0.0.0.0"
-    server.socket_port = 8000
-    server.thread_pool = 30
-    server.max_request_body_size = 0
-    server.socket_timeout = 10000000
     # For SSL Support server.ssl_module = 'pyopenssl' server.ssl_certificate =
     # 'ssl/certificate.crt' server.ssl_private_key = 'ssl/private.key'
     # server.ssl_certificate_chain = 'ssl/bundle.crt' Subscribe this server
-    server.subscribe()
+
+
+    config = {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': settings.STATIC_ROOT,
+        'tools.expires.on': True,
+        'tools.expires.secs': 86400
+    }
+    cherrypy.tree.mount(None, settings.STATIC_URL, {'/': config})
+
+    cherrypy.config.update({
+        "server.socket_host": "0.0.0.0",
+        "server.socket_port": 8000,
+        "server.thread_pool": 30,
+        "server.max_request_body_size": 0,
+        "server.socket_timeout": 10000000})
+
+    # Mount the application
+    cherrypy.tree.graft(application)
+
     cherrypy.engine.start()
     cherrypy.engine.block()
+
